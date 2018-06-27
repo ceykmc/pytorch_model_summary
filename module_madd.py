826e7371c083bc699d1a8334fe503b9d6e573588
lijun <ceykmc@gmail.com>
2018-06-27 13:40:43 +0800

    增加对 transposed convolution 的支持

--- module_madd.py
+++ module_madd.py
@@ -29,6 +29,27 @@ def compute_Conv2d_madd(module, inp, out):
     return total_mul + total_add
 
 
+def compute_ConvTranspose2d_madd(module, inp, out):
+    assert isinstance(module, nn.ConvTranspose2d)
+    assert len(inp.size()) == 4 and len(inp.size()) == len(out.size())
+
+    in_c, in_h, in_w = inp.size()[1:]
+    k_h, k_w = module.kernel_size
+    out_c, out_h, out_w = out.size()[1:]
+    groups = module.groups
+
+    kernel_mul = k_h * k_w * (in_c // groups)
+    kernel_add = kernel_mul - 1 + (0 if module.bias is None else 1)
+
+    kernel_mul_group = kernel_mul * in_h * in_w * (out_c // groups)
+    kernel_add_group = kernel_add * in_h * in_w * (out_c // groups)
+
+    total_mul = kernel_mul_group * groups
+    total_add = kernel_add_group * groups
+
+    return total_mul + total_add
+
+
 def compute_BatchNorm2d_madd(module, inp, out):
     assert isinstance(module, nn.BatchNorm2d)
     assert len(inp.size()) == 4 and len(inp.size()) == len(out.size())
@@ -108,6 +129,8 @@ def compute_Linear_madd(module, inp, out):
 def compute_module_madd(module, inp, out):
     if isinstance(module, nn.Conv2d):
         return compute_Conv2d_madd(module, inp, out)
+    elif isinstance(module, nn.ConvTranspose2d):
+        return compute_ConvTranspose2d_madd(module, inp, out)
     elif isinstance(module, nn.BatchNorm2d):
         return compute_BatchNorm2d_madd(module, inp, out)
     elif isinstance(module, nn.MaxPool2d):
